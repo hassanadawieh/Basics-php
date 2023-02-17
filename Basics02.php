5669c4005c13c96f0403148c867f65ed55c8ea09
hassanadawieh <adawiehhassan@gmail.com>
2023-02-17 16:42:28 +0200

    Basics02 step 2

--- Basics02.php
+++ Basics02.php
@@ -1,4 +1,24 @@
 <?php
 echo "Tomorrow I'll learn PHP global variables. <br>";
 echo "This is a bad command: sudo rm -rf /";
+print("<br>");
+print("<br>");
+print("<br>");
+function greaterFn($num) {
+  if ($num > 30) {
+    return $num . " is greater than 30";
+  } elseif ($num > 20) {
+    return $num . " is greater than 20";
+  } elseif ($num > 10) {
+    return $num . " is greater than 10";
+  } else {
+    return $num . " is less than or equal to 10";
+  }
+}
+
+
+echo greaterFn(40) . "<br>"; // 40 is greater than 30
+echo greaterFn(21) . "<br>"; // 21 is greater than 20
+echo greaterFn(12) . "<br>"; // 12 is greater than 10
+echo greaterFn(8) . "<br>";  // 8 is less than or equal to 10
 ?>
