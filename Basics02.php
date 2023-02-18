1fa00bbb074c91982fdb508a9b3b8e14f3c596ff
hassanadawieh <adawiehhassan@gmail.com>
2023-02-18 09:19:05 +0200

    Basics02 step 5

--- Basics02.php
+++ Basics02.php
@@ -58,5 +58,37 @@ function isArmstrong($num) {
 }
 
 echo isArmstrong(153) ? "true" : "false"; 
+print("<br>");
+print("<br>");
+print("<br>");
 
+
+function word_to_digit($word) {
+  switch ($word) {
+    case 'zero':
+      return 0;
+    case 'one':
+      return 1;
+    case 'two':
+      return 2;
+    case 'three':
+      return 3;
+    case 'four':
+      return 4;
+    case 'five':
+      return 5;
+    case 'six':
+      return 6;
+    case 'seven':
+      return 7;
+    case 'eight':
+      return 8;
+    case 'nine':
+      return 9;
+    default:
+      return "Invalid input"; 
+  }
+}
+
+echo word_to_digit('seven');
 ?>
